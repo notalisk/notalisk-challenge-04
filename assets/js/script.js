ed64341507b424898013090c6e0f38bbe4c41540
notalisk <81662512+notalisk@users.noreply.github.com>
2023-04-11 18:37:23 -0500

    added answer choice functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,6 @@
 var startButton = document.querySelector("#start-button");
 var timerDisplay = document.querySelector("#timer");
 var main = document.querySelector("main");
-var question = document.querySelector(".shown");
-var a1;
-var a2;
-var a3;
-var a4;
 
 // On button click, start game (start timer and change to first question)
 
@@ -14,32 +9,21 @@ var pageCount = 0;
 
 startButton.addEventListener("click", function() {
     nextPage();
-    //setQuestionVars();
 
     secondsLeft = 45;
     var timer = setInterval(function() {
         secondsLeft--;
         timerDisplay.textContent = secondsLeft;
 
-        if(secondsLeft === 0) {
+        if(secondsLeft < 1) {
             // Stops execution of action at set interval
             clearInterval(timer);
+            secondsLeft = 0;
+            timerDisplay.textContent = secondsLeft;
             // Game over :(
             gameOver();
           }
     }, 1000);
-});
-
-// click an answer
-// if the answer is correct, go to the next page
-// if the answer is wrong, subtract from time/score
-question.addEventListener("click", function() {
-
-    if (document.querySelector(".correct")) {
-        nextPage();
-    } else {
-        secondsLeft - 5;
-    }
 });
 
 // Flip to the next page/section/question/screen/whatever-you-call-it
@@ -48,12 +32,23 @@ function nextPage() {
     main.children[pageCount + 1].className = "card question shown";
 }
 
-// set the question and answer (a) variables
-function setQuestionVars() {
-    question = document.querySelector(".shown");
-    a1 = question.children[0];
-    a2 = question.children[1];
-    a3 = question.children[2];
-    a3 = question.children[3];
-    a4 = question.children[4];
-}
+var answers = document.querySelectorAll(".a");
+
+answers.forEach(answer => {
+    answer.addEventListener("click", () => {
+        var question = answer.parentNode;
+
+        var nextQuestion = question.nextElementSibling;
+
+        if (answer.className === "a correct") {
+            question.classList.remove("shown");
+            question.classList.add("hidden");
+            nextQuestion.classList.remove("hidden");
+            nextQuestion.classList.add("shown");
+        } else {
+            console.log("nope!");
+            secondsLeft = secondsLeft - 5;
+        }
+
+    });
+});
